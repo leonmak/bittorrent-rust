2971a7ce0247083ab064de96e23ea895e7cce991
Leon Mak <leonmakansheng@gmail.com>
2024-07-21 16:29:24 +0800

    Add parsing list with len of parsed substring

--- src/main.rs
+++ src/main.rs
@@ -5,37 +5,39 @@ use std::env;
 // use serde_bencode
 
 #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, usize) {
     // If encoded_value starts with a digit, it's a number
     match encoded_value.chars().next() {
         Some(str_len) if str_len.is_digit(10) => {
             let colon_index = encoded_value.find(':').unwrap();
             let number_string = &encoded_value[..colon_index];
             let number = number_string.parse::<i64>().unwrap();
-            let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-            return serde_json::Value::String(string.to_string());
+            let end_idx = colon_index + 1 + number as usize;
+            let string: &str = &encoded_value[colon_index + 1..end_idx];
+            return (serde_json::Value::String(string.to_string()), end_idx);
         }
         Some(i) if i == 'i' => {
             let end_index = encoded_value.find('e').unwrap();
             let num = &encoded_value[1..end_index].parse::<i64>().unwrap();
-            return json!(num);
+            return (json!(num), end_index + 1);
         }
         Some(c) if c == 'l' => {
             let mut list = Vec::new();
             let mut remaining = &encoded_value[1..];
+            let mut len = 0;
             while !remaining.is_empty() {
+                // println!("{remaining:?}");
                 if remaining.starts_with('e') {
-                    remaining = &remaining[1..];
                     break;
                 }
-                let value = decode_bencoded_value(remaining).to_string();
-                let rem = value.to_string().len();
+                let (value, val_len) = decode_bencoded_value(remaining);
+                len += val_len;
                 list.push(value);
-                remaining = &remaining[rem..];
+                remaining = &remaining[val_len..];
             }
-            return json!(list);
+            return (json!(list), len + 2);
         }
-        _ => serde_json::Value::Null,
+        _ => (serde_json::Value::Null, 0),
     }
 }
 
@@ -47,7 +49,7 @@ fn main() {
     if command == "decode" {
         // Uncomment this block to pass the first stage
         let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
+        let (decoded_value, _len) = decode_bencoded_value(encoded_value);
         println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
