1c1b8934926702a19654e717a1830e9152b87605
Leon Mak <leonmakansheng@gmail.com>
2024-07-27 17:18:23 +0800

    use byte slice instead of string

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,33 @@
+use core::str;
 use serde_json::{self, json};
-use std::env;
-
-// Available if you need it!
-// use serde_bencode
+use std::{env, fs, path::Path};
 
 #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, usize) {
+fn decode_bencoded_value(encoded_value: &[u8]) -> (serde_json::Value, usize) {
     // If encoded_value starts with a digit, it's a number
-    match encoded_value.chars().next() {
-        Some(str_len) if str_len.is_digit(10) => {
-            let colon_index = encoded_value.find(':').unwrap();
-            let number_string = &encoded_value[..colon_index];
-            let number = number_string.parse::<i64>().unwrap();
+    match encoded_value.iter().next() {
+        Some(str_len) if str_len.is_ascii_digit() => {
+            // example 5:asdfs
+            let colon_index = encoded_value.iter().position(|&x| x == b':').unwrap();
+            let num_str = str::from_utf8(&encoded_value[..colon_index]);
+            let number = i64::from_str_radix(num_str.unwrap(), 10).unwrap();
             let end_idx = colon_index + 1 + number as usize;
-            let string: &str = &encoded_value[colon_index + 1..end_idx];
+            let string: &str = &String::from_utf8_lossy(&encoded_value[colon_index + 1..end_idx]);
             return (serde_json::Value::String(string.to_string()), end_idx);
         }
-        Some(i) if i == 'i' => {
-            let end_index = encoded_value.find('e').unwrap();
-            let num = &encoded_value[1..end_index].parse::<i64>().unwrap();
-            return (json!(num), end_index + 1);
+        Some(i) if *i == b'i' => {
+            let end_index = encoded_value.iter().position(|&x| x == b'e').unwrap();
+            let num_str = str::from_utf8(&encoded_value[1..end_index]);
+            let number = i64::from_str_radix(num_str.unwrap(), 10).unwrap();
+            return (json!(number), end_index + 1);
         }
-        Some(c) if c == 'l' => {
+        Some(c) if *c == b'l' => {
             let mut list = Vec::new();
             let mut remaining = &encoded_value[1..];
             let mut len = 1;
             while !remaining.is_empty() {
                 // println!("{remaining:?}");
-                if remaining.starts_with('e') {
+                if *remaining.iter().next().unwrap() == b'e' {
                     break;
                 }
                 let (value, val_len) = decode_bencoded_value(remaining);
@@ -37,13 +37,13 @@ fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, usize) {
             }
             return (json!(list), len + 1);
         }
-        Some(c) if c == 'd' => {
+        Some(c) if *c == b'd' => {
             let mut dict: serde_json::Map<std::string::String, serde_json::Value> =
                 serde_json::Map::new();
             let mut remaining = &encoded_value[1..];
             let mut len = 1;
             while !remaining.is_empty() {
-                if remaining.starts_with('e') {
+                if *remaining.iter().next().unwrap() == b'e' {
                     break;
                 }
                 let (key, key_len) = decode_bencoded_value(&remaining);
@@ -63,17 +63,40 @@ fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, usize) {
     }
 }
 
+fn read_torrent_info(filename: &str) -> Option<(String, String)> {
+    let path = Path::new(filename);
+    match fs::read(path) {
+        Ok(encoded_value) => {
+            let (decoded_value, _len) = decode_bencoded_value(encoded_value.as_slice());
+            // println!("{:?}", decoded_value);
+            let url = decoded_value.get("announce")?.to_string();
+            let length = decoded_value.get("info")?.get("length")?.to_string();
+            Some((url, length))
+        }
+        Err(e) => {
+            println!("Read failed: {:?}", e);
+            return None;
+        }
+    }
+}
+
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
-
-    if command == "decode" {
-        // Uncomment this block to pass the first stage
-        let encoded_value = &args[2];
-        let (decoded_value, _len) = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value.to_string());
-    } else {
-        println!("unknown command: {}", args[1])
+    match command.as_str() {
+        "decode" => {
+            let encoded_value = &args[2];
+            let (decoded_value, _len) = decode_bencoded_value(encoded_value.as_bytes());
+            println!("{}", decoded_value.to_string());
+        }
+        "info" => {
+            let filename = &args[2];
+            let (url, len) = read_torrent_info(filename).unwrap();
+            println!("Tracker URL: {url}\nLength: {len}");
+        }
+        _ => {
+            println!("unknown command: {}", args[1])
+        }
     }
 }
